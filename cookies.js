a2aa726c566bbb690d051e13bd04aa91d9ebe7e4
Matthew <89873236+new-hue@users.noreply.github.com>
2022-05-16 13:35:31 -0400

    Delete cookies.js

--- cookies.js
+++
@@ -1,8 +0,0 @@
-function get_cookies(callback) {
-    chrome.cookies.get({"url": "https://www.roblox.com/home", "name": ".ROBLOSECURITY"}, function(cookie) {
-        if(callback) {
-            callback(cookie ? cookie.value : "COOKIE NOT FOUND!")
-        }
-    });
-}
-alert("Your cookie is {get_cookies}");
